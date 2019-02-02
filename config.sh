4e6a0d30f14ebf895a93a750e5429d4c66328bfa
Prakersh Maheshwari <prakersh@lap-prakershmaheshwari.coriolis.in>
2019-02-03 01:07:05 +0530

    rev 4

--- config.sh
+++ config.sh
@@ -1,9 +1,9 @@
 
-#SELF_IP=
-#OPTHER_IP=
+SELF_IP=172.20.4.191
+OPTHER_IP=172.20.4.20
 sed -i 's/thisserveraddresss/$SELF_IP/g' video/main.py
 
-sed -i 's/thisserveraddresss/$SELF_IP/g' audio/server.py
+sed -i 's/thisserveraddresss/$SELF_IP/g' sound/server.py
 
-sed -i 's/otherserveraddresss/$OTHER_IP/g' audio/client.py
+sed -i 's/otherserveraddresss/$OTHER_IP/g' sound/client.py
 
