be2136284103a61cdd24411a16cb54e32e27be98
AnirudhM <anirudh.mavinkurve@gmail.com>
2019-02-03 18:08:48 +0530

    Changes to scripts

--- setup.sh
+++ setup.sh
@@ -34,3 +34,10 @@ sed -ie "s/thisserveraddress/$SELF_IP/g" /opt/ctpl_link/video/main.py
 sed -ie "s/thisserveraddress/$SELF_IP/g" /opt/ctpl_link/sound/server.py
 sed -ie "s/otherserveraddress/$OTHER_IP/g" /opt/ctpl_link/sound/client.py
 
+useradd -d /home/vlink vlink
+mkdir -p  /home/vlink
+chown -R vlink:vlink /home/vlink
+echo "vlink:ssl12345" > /var/tmp/pass.txt
+cat /var/tmp/pass.txt | chpasswd
+chmod 777  /var/log/ctpl_link
+
