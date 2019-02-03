cb210a69251ce586f7efac52bdd552002f41b6c0
Prakersh Maheshwari <prakersh@lap-prakershmaheshwari.corp.coriolis.in>
2019-02-03 17:18:54 +0530

    UPdate

--- ctpl_link_initial.sh
+++ ctpl_link_initial.sh
@@ -16,6 +16,7 @@ echo -n "Enter IP of Client: "
 read CLIENT_IP
 echo -n "Enter Root Password of Client Machine: "
 read -s CLIENT_PASSWORD
+echo
 echo -n "Enter Root Password of Server Machine: "
 read -s SERVER_PASSWORD
 sudo apt-get -y install expect sshpass git
@@ -23,7 +24,7 @@ cd /opt
 sudo git clone https://github.com/akgjec/ctpl_link
 /opt/ctpl_link/tools/set_passwordlessSSH.sh -host ${CLIENT_IP} -user ${CLIENT_USERNAME} -pass ${CLIENT_PASSWORD}
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa_vlink -o PreferredAuthentications=publickey -o BatchMode=yes -p 22 $CLIENT_USERNAME@$CLIENT_IP  "rm -rf /var/tmp/ctpl_link  /opt/ctpl_link;  mkdir -p /var/tmp/ctpl_link /var/log/ctpl_link; apt-get -y -y install expect sshpass git"
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa_vlink -o PreferredAuthentications=publickey -o BatchMode=yes -p 22 $CLIENT_USERNAME@$CLIENT_IP  "cd /opt; clone https://github.com/akgjec/ctpl_link"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa_vlink -o PreferredAuthentications=publickey -o BatchMode=yes -p 22 $CLIENT_USERNAME@$CLIENT_IP  "cd /opt; git clone https://github.com/akgjec/ctpl_link"
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa_vlink -o PreferredAuthentications=publickey -o BatchMode=yes -p 22 $CLIENT_USERNAME@$CLIENT_IP " /opt/ctpl_link/tools/set_passwordlessSSH.sh -host ${SERVER_IP} -user ${SERVER_USERNAME} -pass ${SERVER_PASSWORD}"
 
 sudo /opt/ctpl_link/setup.sh $SERVER_IP $CLIENT_IP  | tee /var/log/ctpl_link/install.log
