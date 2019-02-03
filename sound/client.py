f2fc3523d9e0bbabc9fa9178a6e49d5de6367ded
Prakersh Maheshwari <prakersh@lap-prakershmaheshwari.corp.coriolis.in>
2019-02-03 18:45:19 +0530

    REv

--- sound/client.py
+++ sound/client.py
@@ -1,6 +1,7 @@
 import pyaudio
 import socket
 from threading import Thread
+from time import sleep
 
 frames = []
 
@@ -20,28 +21,34 @@ def record(stream, CHUNK):
         except Exception as Err:
             print(Err)
 
+def start_client():
+    try:
+        CHUNK = 1024
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 2
+        RATE = 44100
+
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format = FORMAT,
+                        channels = CHANNELS,
+                        rate = RATE,
+                        input = True,
+                        frames_per_buffer = CHUNK,
+                        )
+
+        Tr = Thread(target = record, args = (stream, CHUNK,))
+        Ts = Thread(target = udpStream)
+        Tr.setDaemon(True)
+        Ts.setDaemon(True)
+        Tr.start()
+        Ts.start()
+        Tr.join()
+        Ts.join()
+    except Exception as Err:
+        print(Err)
+        sleep(0.5)
+        start_client()
+
 if __name__ == "__main__":
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 2
-    RATE = 44100
-
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format = FORMAT,
-                    channels = CHANNELS,
-                    rate = RATE,
-                    input = True,
-                    frames_per_buffer = CHUNK,
-                    )
-
-    Tr = Thread(target = record, args = (stream, CHUNK,))
-    Ts = Thread(target = udpStream)
-    Tr.setDaemon(True)
-    Ts.setDaemon(True)
-    Tr.start()
-    Ts.start()
-    Tr.join()
-    Ts.join()
-
-
+    start_client()
