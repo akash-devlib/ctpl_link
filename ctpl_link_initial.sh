0dfb4d232c42bf722edf0a6d4bed2dbc0f63f76e
Prakersh Maheshwari <prakersh@lap-prakershmaheshwari.corp.coriolis.in>
2019-02-03 22:20:01 +0530

    rev 2

--- ctpl_link_initial.sh
+++ ctpl_link_initial.sh
@@ -28,5 +28,5 @@ ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa_vlink -o PreferredAuthentications=publickey -o BatchMode=yes -p 22 $CLIENT_USERNAME@$CLIENT_IP " /opt/ctpl_link/tools/set_passwordlessSSH.sh -host ${SERVER_IP} -user ${SERVER_USERNAME} -pass ${SERVER_PASSWORD}"
 
 nohup /opt/ctpl_link/setup.sh $SERVER_IP $CLIENT_IP  > /var/log/ctpl_link/install.log 2>&1 &
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa_vlink -o PreferredAuthentications=publickey -o BatchMode=yes -p 22 $CLIENT_USERNAME@$CLIENT_IP " sudo /opt/ctpl_link/setup.sh $CLIENT_IP $SERVER_IP | tee /var/log/ctpl_link/install.log "
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa_vlink -o PreferredAuthentications=publickey -o BatchMode=yes -p 22 $CLIENT_USERNAME@$CLIENT_IP " nohup /opt/ctpl_link/setup.sh $CLIENT_IP $SERVER_IP > /var/log/ctpl_link/install.log 2>&1 &"
 
