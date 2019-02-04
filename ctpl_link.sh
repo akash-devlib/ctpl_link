6be350416172a8a9aed06d8c275689725076c915
Prakersh Maheshwari <prakersh@lap-prakershmaheshwari.corp.coriolis.in>
2019-02-04 15:03:51 +0530

    Moved to chrome

--- ctpl_link.sh
+++ ctpl_link.sh
@@ -2,8 +2,9 @@ LOG_DIR=/var/log/ctpl_link
 VIDEO_URL=otherserveraddress:5000/
 
 export DISPLAY=:0
-nohup python3 video/main.py > ${LOG_DIR}/video.log  2>&1 &
-nohup python3 sound/server.py  > ${LOG_DIR}/audio_server.log  2>&1 &
-nohup python3 sound/client.py  > ${LOG_DIR}/audio_client.log  2>&1 &
+nohup python3 /opt/ctpl_link/video/main.py > ${LOG_DIR}/video.log  2>&1 &
+nohup python3 /opt/ctpl_link/sound/server.py  > ${LOG_DIR}/audio_server.log  2>&1 &
+nohup python3 /opt/ctpl_link/sound/client.py  > ${LOG_DIR}/audio_client.log  2>&1 &
+sleep 10
 nohup google-chrome ${VIDEO_URL} &
 
